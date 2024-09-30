c7500ffa8452bcbe8ef9498e03b4a7a49df19090
ardianagoes <ardianagoes83@gmail.com>
2024-09-29 20:29:35 -0400

    wrote concatArray method, added another testcase, and cleaned up output

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -4,7 +4,7 @@ public class MyArrays {
     int[] copy = returnCopy(array);
     boolean sameValue = true;
 
-    System.out.println("Original Array: " + arrayToString(array));
+    System.out.println("\nOriginal Array: " + arrayToString(array));
     System.out.println("Copied Array: " + arrayToString(copy));
 
     //Check if arrays have the same values
@@ -17,15 +17,22 @@ public class MyArrays {
 
     //Check if arrays are the same
     array[0] = 99;
-    System.out.println("Modified Array: " + arrayToString(array));
+    System.out.println("\nModified Array: " + arrayToString(array));
     System.out.println("Copied Array: " + arrayToString(copy));
 
-    /*int[] array1 = {0, 1, 2, 3, 4, 5};
+    int[] array1 = {0, 1, 2, 3, 4, 5};
     int[] array2 = {6, 7, 8, 9};
 
-    System.out.println("Array 1: " + arrayToString(array1));
+    System.out.println("\nArray 1: " + arrayToString(array1));
     System.out.println("Array 2: " + arrayToString(array2));
-    System.out.println("Array 1 + 2: " + arrayToString(concatArray(array1, array2)));*/
+    System.out.println("Array 1 + 2: " + arrayToString(concatArray(array1, array2)));
+
+    int[] array12 = {0, 1, 2, 3};
+    int[] array22 = {4, 5, 6, 7, 8, 9, 10};
+
+    System.out.println("\nArray 1: " + arrayToString(array12));
+    System.out.println("Array 2: " + arrayToString(array22));
+    System.out.println("Array 1 + 2: " + arrayToString(concatArray(array12, array22)));
   }
 
   public static String arrayToString(int[] nums){
@@ -43,6 +50,17 @@ public class MyArrays {
     int[] newArray = new int[ary.length];
     for (int i = 0; i < newArray.length; i++){
       newArray[i] = ary[i];
+    }
+    return newArray;
+  }
+
+  public static int[] concatArray(int[]ary1,int[]ary2){
+    int[] newArray = new int[ary1.length + ary2.length];
+    for (int i = 0; i < ary1.length; i++){
+      newArray[i] = ary1[i];
+    }
+    for (int i = 0; i < ary2.length; i++){
+      newArray[ary1.length + i] = ary2[i];
     }
     return newArray;
   }
