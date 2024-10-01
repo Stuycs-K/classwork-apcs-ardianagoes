eb9e06f0622d3c37686300252dc83eb9f378bd42
ardianagoes <ardianagoes83@gmail.com>
2024-09-30 22:19:53 -0400

    added more test cases for the methods

---
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -0,0 +1,98 @@
+// Maxim Argat (maxima5@nycstudents.net) and Ardian Agoes (ardiana@nycstudents.net)
+
+public class ArrayMethods
+{
+    public static void main(String[] args)
+    {
+        int[] array1D = new int[]{2, 3, 4, 9};
+        int[] array1D2 = new int[]{};
+        int[][] array1 = new int[][]{{2, 3, 4}, {5, 6, 7}, {2, 4, 9}};
+        int[][] array2 = new int[][]{{2, 7}, {3, 6, 1}, {7}};
+        int[][] array3 = new int[][]{{11, 12}, {}, {20, 21}};
+
+        // 1D Array to String Method Tests
+        System.out.println("\n1D Array1D as String: ");
+        System.out.println(arrToString(array1D));
+        System.out.println("\n1D Array1D2 as String: ");
+        System.out.println(arrToString(array1D2));
+        // 1D Array to String Method Tests
+
+        // 2D Array to String Method Tests
+        System.out.println("\nArray 1 as a String: ");
+        System.out.println(arrToString(array1));
+        System.out.println("\nArray 2 as a String: ");
+        System.out.println(arrToString(array2));
+        System.out.println("\nArray 3 as a String: ");
+        System.out.println(arrToString(array3));
+        // 2D Array to String Method Tests
+
+        // 2D Array Sum Method Tests
+        System.out.println("\nSum of the values in Array 1: " + arr2DSum(array1));
+        System.out.println("Sum of the values in Array 2: " + arr2DSum(array2));
+        System.out.println("Sum of the values in Array 3: " + arr2DSum(array3));
+        // 2D Array Sum Method Tests
+
+        int[][] array4 = new int[][]{{1, 2, 3}, {4, 5, 6}};
+        int[][] array5 = new int[][]{{1, 4},{2, 5},{3, 6}};
+        int[][] array6 = new int[][]{{6, 9},{4, 0}};
+
+        // swapRC Method Tests
+        System.out.println("\nOriginal Array 4: " + arrToString(array4) + " --> " + arrToString(swapRC(array4))); // --> {{1, 4},{2, 5},{3, 6}}
+        System.out.println("Original Array 5: " + arrToString(array5) + " --> " + arrToString(swapRC(array5))); // --> {{1, 2, 3}, {4, 5, 6}}
+        System.out.println("Original Array 5: " + arrToString(array6) + " --> " + arrToString(swapRC(array6))); // --> {{6, 9},{4, 0}}
+        // swapRC Method Tests
+    }
+
+    public static String arrToString(int[] array)
+    {
+        String s = "[";
+        for(int i = 0; i < array.length; i++)
+        {
+            s += array[i] + ", ";
+        }
+        if(array.length > 0)
+            return s.substring(0, s.length() - 2) + "]";
+        return s + "]";
+    }
+
+    public static String arrToString(int[][] array)
+    {
+        String s = "[";
+        for(int i = 0; i < array.length; i++)
+        {
+            s += arrToString(array[i]) + ", ";
+        }
+        if(array.length > 0)
+            return s.substring(0, s.length() - 2) + "]";
+        return s + "]";
+    }
+
+    public static int arr2DSum(int[][] array)
+    {
+        int sum = 0;
+        for(int i = 0; i < array.length; i++)
+        {
+            int[] subarray = array[i];
+            for(int j = 0; j < subarray.length; j++)
+            {
+                sum += subarray[j];
+            }
+        }
+        return sum;
+    }
+
+    public static int[][] swapRC(int[][] nums)
+    {
+      int rows = nums[0].length;
+      int columns = nums.length;
+      int[][] returnArray = new int[rows][columns];
+      for (int n = 0; n < columns; n++)
+      {
+        for (int i = 0; i < rows; i++)
+        {
+            returnArray[i][n] = nums[n][i];
+        }
+      }
+      return returnArray;
+    }
+}
