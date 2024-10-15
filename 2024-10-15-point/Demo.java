fa67441382ff03e438934d5a532f0d76f0f48ca9
ardianagoes <ardianagoes83@gmail.com>
2024-10-15 19:10:45 -0400

    copied over work done on partner's computer

---
+++ 2024-10-15-point/Demo.java
@@ -0,0 +1,42 @@
+public class Demo {
+
+ /*
+ Write this method third. Test it then move on
+ to the others.
+ */
+ public static double distance(Point a, Point b) {
+   return Math.sqrt(Math.pow(b.getX() - a.getX(), 2) + Math.pow(b.getY() - a.getY(), 2));
+ }
+
+ public static void main(String[]args) {
+   Point p1 = new Point(1,1);
+   Point p2 = new Point(-1,-1);
+   Point p3 = new Point(3,4);
+   Point p4 = new Point(p3);
+   System.out.println( p3);
+   System.out.println(p4);
+   System.out.println( distance(p1,p2));
+   System.out.println( Point.distance(p1,p2));
+   System.out.println( p1.distanceTo(p2));
+   System.out.println( distance(p2,p1));
+   System.out.println( Point.distance(p2,p1));
+   System.out.println( p2.distanceTo(p1));
+   System.out.println( distance(p3,p4));
+   System.out.println( Point.distance(p3,p4));
+   System.out.println( p3.distanceTo(p4));
+
+   Point triPoint1 = new Point(0, 0);
+   Point triPoint2 = new Point(2, 0);
+
+   // 1^2 + x^2 = 2^2
+   // 1 + x^2 = 4
+   // x^2 = 3
+   Point triPoint3 = new Point(1, Math.sqrt(3));
+
+   System.out.println("Side Length Between Point 1 and 2: " + distance(triPoint1, triPoint2));
+   System.out.println("Side Length Between Point 1 and 3: " + triPoint1.distanceTo(triPoint3));
+   System.out.println("Side Length Between Point 2 and 3: " + Point.distance(triPoint2, triPoint3));
+
+
+ }
+}
