616ede3eec43ff1fe62da00033b7821b9ba85a86
ardianagoes <ardianagoes83@gmail.com>
2024-10-15 19:24:32 -0400

    more test cases for Point constructor

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -13,17 +13,30 @@ public class Demo {
    Point p2 = new Point(-1,-1);
    Point p3 = new Point(3,4);
    Point p4 = new Point(p3);
-   System.out.println( p3);
-   System.out.println(p4);
-   System.out.println( distance(p1,p2));
-   System.out.println( Point.distance(p1,p2));
-   System.out.println( p1.distanceTo(p2));
-   System.out.println( distance(p2,p1));
-   System.out.println( Point.distance(p2,p1));
-   System.out.println( p2.distanceTo(p1));
-   System.out.println( distance(p3,p4));
-   System.out.println( Point.distance(p3,p4));
-   System.out.println( p3.distanceTo(p4));
+   Point p5 = new Point(100, 1);
+   Point p6 = new Point(p5);
+   Point p7 = new Point(100, 100);
+   Point p8 = new Point(p7);
+
+   System.out.println("\n-----Testing Point(Point other) constructor-----");
+   System.out.println("\nOriginal Point: " + p3);
+   System.out.println("Cloned Point: " + p4);
+   System.out.println("\nOriginal Point: " + p5);
+   System.out.println("Cloned Point: " + p6);
+   System.out.println("\nOriginal Point: " + p7);
+   System.out.println("Cloned Point: " + p8);
+   System.out.println("\n-----Testing Point(Point other) constructor-----");
+
+
+   System.out.println(distance(p1,p2));
+   System.out.println(Point.distance(p1,p2));
+   System.out.println(p1.distanceTo(p2));
+   System.out.println(distance(p2,p1));
+   System.out.println(Point.distance(p2,p1));
+   System.out.println(p2.distanceTo(p1));
+   System.out.println(distance(p3,p4));
+   System.out.println(Point.distance(p3,p4));
+   System.out.println(p3.distanceTo(p4));
 
    Point triPoint1 = new Point(0, 0);
    Point triPoint2 = new Point(2, 0);
@@ -31,6 +44,7 @@ public class Demo {
    // 1^2 + x^2 = 2^2
    // 1 + x^2 = 4
    // x^2 = 3
+
    Point triPoint3 = new Point(1, Math.sqrt(3));
 
    System.out.println("Side Length Between Point 1 and 2: " + distance(triPoint1, triPoint2));
