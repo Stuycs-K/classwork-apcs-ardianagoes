b7cf0055132e5ea59a7913cf172c71b8d06856bd
ardianagoes <ardianagoes83@gmail.com>
2024-11-04 10:10:34 -0500

    current progress -- worked on counttrianglesa and checktriangle

--- 2024-10-31-Advent/TriangleTester.java
+++ 2024-10-31-Advent/TriangleTester.java
@@ -1,16 +1,45 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class TriangleTester {
 
   public static void main(String[] args){
-
+    System.out.println(countTrianglesA("inputTri.txt"));
 
   }
 
   public static int countTrianglesA(String filename){
-    
+    try {
+      File file = new File("inputTri.txt");
+      Scanner input = new Scanner(file);
+      int counter = 0;
+      while (input.hasNextLine()){
+        String currentLine = input.nextLine();
+        String[] currentArray = new String[3];
+        currentArray = currentLine.split("");
+          if (checkTriangle(currentArray)){
+            counter++;
+          }
+      }
+      return counter;
+    }  catch (FileNotFoundException ex) {
+        //File not found what should you do?
+        System.out.println("File not found");
+        return 0; //you can return from a void function just don't put a value.
+      }
   }
 
-  public static boolean checkTriangle(int side1, int side2, int side3){
-    return (Math.max(side1, side2, side3) < (side1 + side2 + side3 - Math.max(side1, side2, side3)));
+  public static boolean checkTriangle(String[] array){
+    int[] intArray = new int[3];
+    for (int i = 0; i < array.length; i++){
+      intArray[i] = Integer.parseInt(array[i]);
+    }
+    return (intArray[0] + intArray[1] < intArray[2] && intArray[2] + intArray[1] < intArray[0] && intArray[0] + intArray[2] < intArray[1]);
+
+    //return (Math.max(Integer.parseInt(array[0]), Integer.parseInt(array[1]), Integer.parseInt(array[2])) < (Integer.parseInt(array[0]) + Integer.parseInt(array[1]) + Integer.parseInt(array[2]) - Math.max(Integer.parseInt(array[0]), Integer.parseInt(array[1]), Integer.parseInt(array[2]))));
   }
 
 }
+
+//
