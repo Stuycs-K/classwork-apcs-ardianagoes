143a79a5d05fc49e456728b534a7f52fe8faeded
ardianagoes <ardianagoes83@gmail.com>
2024-11-04 09:50:34 -0500

    wrote checktriangle method

---
+++ 2024-10-31-Advent/TriangleTester.java
@@ -0,0 +1,16 @@
+public class TriangleTester {
+
+  public static void main(String[] args){
+
+
+  }
+
+  public static int countTrianglesA(String filename){
+    
+  }
+
+  public static boolean checkTriangle(int side1, int side2, int side3){
+    return (Math.max(side1, side2, side3) < (side1 + side2 + side3 - Math.max(side1, side2, side3)));
+  }
+
+}
