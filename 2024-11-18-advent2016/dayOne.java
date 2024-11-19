651147d4a220b7714803fc683e7ae7d2861f3ab6
ardianagoes <ardianagoes83@gmail.com>
2024-11-18 21:44:28 -0500

    current progress

--- 2024-11-18-advent2016/dayOne.java
+++ 2024-11-18-advent2016/dayOne.java
@@ -7,11 +7,12 @@ import java.util.ArrayList;
 public class dayOne {
 
   public static void main(String[] args){
-    System.out.println(Arrays.toString(countBlocks("text.txt")));
+    System.out.println(countBlocks("text.txt"));
   }
 
-  public static String[] countBlocks(String filename){
+  public static int countBlocks(String filename){
     try {
+      char direction = 'n';
       int horizontalDistance = 0;
       int verticalDistance= 0;
       File file = new File("text.txt");
@@ -21,10 +22,53 @@ public class dayOne {
         arrayAsString += input.next();
           }
       String[] array = arrayAsString.split(",");
+      for (int i = 0; i < array.length; i++){
+        if (array[i].charAt(0) == 'R'){
+          if (direction == ('n')){
+            direction = 'e';
+          }
+          else if (direction == 'e'){
+            direction = 's';
+          }
+          else if (direction == 's'){
+            direction = 'w';
+          }
+          else if (direction == 'w') {
+            direction = 'n';
+          }
+        }
+        else if (array[i].charAt(0) == 'L'){
+          if (direction == ('n')){
+            direction = 'w';
+          }
+          else if (direction == 'w'){
+            direction = 's';
+          }
+          else if (direction == 's'){
+            direction = 'e';
+          }
+          else if (direction == 'e') {
+            direction = 'n';
+          }
+        }
+        if (direction == 'n'){
+          verticalDistance += Integer.parseInt(array[i].substring(1));
+        }
+        if (direction == 's'){
+          verticalDistance -= Integer.parseInt(array[i].substring(1));
+        }
+        if (direction == 'e'){
+          horizontalDistance += Integer.parseInt(array[i].substring(1));
+        }
+        if (direction == 'w'){
+          verticalDistance -= Integer.parseInt(array[i].substring(1));
+        }
+      }
+      return Math.abs(horizontalDistance) + Math.abs(verticalDistance);
       } catch (FileNotFoundException ex) {
         //File not found what should you do?
         System.out.println("File not found");
-        return new String[1]; //you can return from a void function just don't put a value.
+        return 0; //you can return from a void function just don't put a value.
       }
   }
 }
