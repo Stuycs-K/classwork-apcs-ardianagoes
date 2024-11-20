008d16004e721ce3da31f26df11ddbdc3fb21bb1
ardianagoes <ardianagoes83@gmail.com>
2024-11-20 10:14:53 -0500

    created day 4 java file and text file

---
+++ 2024-11-18-advent2016/dayFour.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class dayFour {
+  public static void main(String[] args){
+
+  }
+  public static int roomChecker(String filename){
+    try {
+      File file = new File("")
+    }
+  }
+}
