10eace585d24f97f00b70932eedef016c0a4d029
ardianagoes <ardianagoes83@gmail.com>
2024-11-21 21:14:55 -0500

    laptop progress added

--- 2024-11-18-advent2016/dayFour.java
+++ 2024-11-18-advent2016/dayFour.java
@@ -6,11 +6,26 @@ import java.util.ArrayList;
 
 public class dayFour {
   public static void main(String[] args){
-
+    System.out.println(roomChecker("dayFour.txt"));
   }
   public static int roomChecker(String filename){
     try {
-      File file = new File("")
+      File file = new File("dayFour.txt");
+      Scanner input = new Scanner(file);
+      ArrayList<String> letters = new ArrayList<String>();
+      ArrayList<String> numbers = new ArrayList<String>();
+      while (input.hasNextLine()){
+        String s = input.nextLine();
+        for (int i = 0; i < s.length(); i++){
+          if (Character.isLetter(s.charAt(i))){
+            return 1;
+          }
+        }
+        }
+        return 2;
+        } catch (FileNotFoundException ex){
+          System.out.println("File not found");
+          return 0;
+        }
+      }
     }
-  }
-}
