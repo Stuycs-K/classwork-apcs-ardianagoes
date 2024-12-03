5b53e04273fd839a4ae11861b4c6539e2f21387b
ardianagoes <ardianagoes83@gmail.com>
2024-12-03 10:08:19 -0500

    more colors

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -12,6 +12,13 @@ public class ColorDemo{
     System.out.print(" also blue because no reset... also your terminal is blue now too...");
     System.out.println("\u001b[0m");
     System.out.println("Hello");
-
+    for(int r = 0; r < 256; r+=1){
+      for(int g = 0; g <= 256; g+=1){
+        for(int b = 0; b <= 256; b+=1){
+          System.out.print("\u001b[38;2;"+r+";"+g+";"+b+";7m.");
+        }
+      }
+        System.out.println();
+    }
   }
 }
