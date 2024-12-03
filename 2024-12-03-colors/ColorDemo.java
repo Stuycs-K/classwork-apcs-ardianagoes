aabc90c5f9e28ef96235059af2c8a913b6ba3c57
ardianagoes <ardianagoes83@gmail.com>
2024-12-03 10:05:02 -0500

    added class stuff into color

---
+++ 2024-12-03-colors/ColorDemo.java
@@ -0,0 +1,17 @@
+public class ColorDemo{
+
+  public static void main(String[] args){
+
+    System.out.print("\u001b[31m");
+    System.out.println("Hello World");
+    System.out.print("\u001b[0m");
+    System.out.print("\u001b[31mRed");
+    System.out.print(" fish ");
+    System.out.print("\u001b[34mBlue");
+    System.out.print(" fish ");
+    System.out.print(" also blue because no reset... also your terminal is blue now too...");
+    System.out.println("\u001b[0m");
+    System.out.println("Hello");
+
+  }
+}
