82f21f2acde6c0ef3ff4d285886d4899b06e7d07
ardianagoes <ardianagoes83@gmail.com>
2024-12-05 06:48:58 -0500

    added the ints

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -1,21 +1,20 @@
+import java.util.Random;
+
+
 public class Driver {
 
   public static void main(String[] args){
     System.out.print(Text.CLEAR_SCREEN); // Clear the screen
     System.out.print(Text.HIDE_CURSOR); // Hide the cursor
-
     makeBorder();
-
+    randomInts();
     Text.go(31, 1); // Move the cursor to the end to avoid overwriting
     System.out.print(Text.RESET); // Reset terminal colors and show the cursor
   }
 
 
-  public static void makeBorder(){
-    int borderColor = Text.BRIGHT;
-    int borderForeground = Text.BLUE;
-    int borderBackground = Text.background(Text.CYAN);
-    Text.color(borderColor, borderForeground, borderBackground);
+    public static void makeBorder(){
+    Text.color(Text.BRIGHT, Text.BLUE, Text.background(Text.CYAN));
     for (int col = 1; col <= 80; col++) {
         Text.go(1, col);
         System.out.print("*");
@@ -28,5 +27,29 @@ public class Driver {
        Text.go(row, 80);
        System.out.print("*");
       }
-   }
+    }
+
+   public static void randomInts(){
+     Random random = new Random();
+     int[] numbers = new int[3];
+     for (int i = 0; i < 3; i++){
+        numbers[i] = random.nextInt(100);
+     }
+     int[] position = {20, 40, 60};
+     for (int i = 0; i < 3; i++){
+        if (numbers[i] < 25){
+          Text.color(Text.BRIGHT, Text.RED, Text.background(Text.BLACK));
+        }
+        else if (numbers[i] > 75){
+          Text.color(Text.BRIGHT, Text.GREEN, Text.background(Text.BLACK));
+        }
+        else {
+          Text.color(Text.BRIGHT, Text.WHITE, Text.background(Text.BLACK));
+        }
+        Text.go(2, position[i]);
+        System.out.println(numbers[i]);
+      }
+    }
+
+
 }
